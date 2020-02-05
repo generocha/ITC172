93085b65e84896d263ef964786f2ee6c16ae6da3
generocha <gene@generocha.com>
2020-02-05 11:18:59 -0800

    add inital view and detail pages

--- PythonClub/Club/views.py
+++ PythonClub/Club/views.py
@@ -1,5 +1,5 @@
 from .models import Meeting, Resource, MeetingMinutes,Event
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 def index (request):
@@ -9,3 +9,18 @@ def gettypes(request):
     resource_list=Resource.objects.all()
     return render(request, 'club/resources.html' ,{'resource_list' : resource_list})
 
+def getmeetings(request):
+    meetings_list=Meeting.objects.all()
+    return render(request, 'club/meetings.html', {'meetings_list': meetings_list})
+
+def meetingdetails(request, id):
+    meet=get_object_or_404(Meeting, pk=id)
+    meet=Meeting.objects.get(pk=id)
+    #discount=prod.memberdiscount
+    #reviews=Review.objects.filter(product=id).count()
+    context={
+        'meet' : meet,
+        #'discount' : discount,
+        #'reviews' : reviews,
+    }
+    return render(request, 'club/meetingdetails.html', context=context)
