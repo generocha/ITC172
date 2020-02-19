56587c205b9f99c2e7d3da60590a5eb57e2d018b
generocha <gene@generocha.com>
2020-02-19 11:48:57 -0800

    add meeting and resource forms, newmeeting.html, newresource.html and form test for resource form

--- PythonClub/Club/views.py
+++ PythonClub/Club/views.py
@@ -1,26 +1,58 @@
-from .models import Meeting, Resource, MeetingMinutes,Event
-from django.shortcuts import render,get_object_or_404
+from .models import Meeting, Resource, MeetingMinutes, Event
+from django.shortcuts import render, get_object_or_404
+from .forms import MeetingForm, ResourceForm
 
 # Create your views here.
-def index (request):
+
+
+def index(request):
     return render(request, 'club/index.html')
 
+
 def gettypes(request):
-    resource_list=Resource.objects.all()
-    return render(request, 'club/resources.html' ,{'resource_list' : resource_list})
+    resource_list = Resource.objects.all()
+    return render(request, 'club/resources.html', {'resource_list': resource_list})
+
 
 def getmeetings(request):
-    meetings_list=Meeting.objects.all()
+    meetings_list = Meeting.objects.all()
     return render(request, 'club/meetings.html', {'meetings_list': meetings_list})
 
+
 def meetingdetails(request, id):
-    meet=get_object_or_404(Meeting, pk=id)
-    meet=Meeting.objects.get(pk=id)
-    #discount=prod.memberdiscount
-    #reviews=Review.objects.filter(product=id).count()
-    context={
-        'meet' : meet,
-        #'discount' : discount,
-        #'reviews' : reviews,
+    meet = get_object_or_404(Meeting, pk=id)
+    meet = Meeting.objects.get(pk=id)
+    # discount=prod.memberdiscount
+    # reviews=Review.objects.filter(product=id).count()
+    context = {
+        'meet': meet,
+        # 'discount' : discount,
+        # 'reviews' : reviews,
     }
     return render(request, 'club/meetingdetails.html', context=context)
+
+
+def newMeeting(request):
+    form = MeetingForm
+    if request.method == 'POST':
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=True)
+            post.save()
+            form = MeetingForm()
+    else:
+        form = MeetingForm()
+    return render(request, 'club/newmeeting.html', {'form': form})
+
+
+def newResource(request):
+    form = ResourceForm
+    if request.method == 'POST':
+        form = ResourceForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=True)
+            post.save()
+            form = ResourceForm()
+    else:
+        form = ResourceForm()
+    return render(request, 'club/newresource.html', {'form': form})
